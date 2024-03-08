5b1b560f7947f6cc1d2dd21c4ce412331dc7f055
Tobias Kietreiber <tobias.kietreiber@fhstp.ac.at>
2024-03-08 08:50:22 +0100

    feat: adjust to EndeavourOS and add dotfiles

--- install.bash
+++ install.bash
@@ -1,23 +1,40 @@
+BASEDIR=$(pwd)
+
 # update
 sudo pacman -Syyu
 
-# enable AUR in pamac
-sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf
-
 # install general programs (some redundant and usually pre-installed - just to make sure they are there)
-sudo pacman -Syu --needed firefox vim kitty zsh rofi $(pacman -Ssq texlive-*) signal-desktop telegram-desktop base-devel nemo
-pamac install polybar arandr pulseaudio pavucontrol
+sudo pacman -Syu --needed firefox vim kitty zsh rofi $(pacman -Ssq texlive-*) signal-desktop telegram-desktop base-devel nemo rsync
+yay install polybar arandr pulseaudio pavucontrol
+
+# snap
+mkdir ~/git
+cd ~/git
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap # https://stackoverflow.com/questions/68565756
+cd ~
+
 sudo snap install code --classic
+sudo snap install obisidian --classic
 sudo snap install teams-for-linux
 
 # change default shell
 chsh -s $(which zsh)
 
+# oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
 # mamba
 wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3-$(uname)-$(uname -m).sh -b
 bash ~/miniforge3/condabin/mamba init bash zsh
+rm Miniforge-*
 
 
 # firefox extensions
@@ -29,10 +46,20 @@ firefox extensions/bitwarden_password_manager-latest.xpi # needs to be clicked!
 rm -rf extensions
 
 # code extensions
-code --install-extension ms-python.python
-code --install-extension ms-toolsai.jupyter
-code --install-extension James-Yu.latex-workshop
-code --install-extension PKief.material-icon-theme
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension vscodevim.vim
+snap run code --install-extension ms-python.python
+snap run code --install-extension ms-toolsai.jupyter
+snap run code --install-extension James-Yu.latex-workshop
+snap run code --install-extension PKief.material-icon-theme
+snap run code --install-extension Equinusocio.vsc-material-theme
+snap run code --install-extension vscodevim.vim
+
+# copy dotfiles
+rsync -a $BASEDIR/.config/ ~/.config/
+
+# kitty themes
+cd ~/git
+git clone https://github.com/dexpota/kitty-themes
+ln -s ~/git/kitty-themes/themes/MaterialDark.conf ~/.config/kitty/theme.conf
+echo "include ./theme.conf" >> ~/.config/kitty/kitty.conf
+
 
