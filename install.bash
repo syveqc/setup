cb2ce7c588063986e9302774817a1240b23bf86f
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-03-19 21:46:17 +0100

    feat: add sioyek

--- install.bash
+++ install.bash
@@ -18,7 +18,7 @@ sudo $PACKAGE_MANAGER -Syyu --noconfirm
 
 # install general programs (some redundant and usually pre-installed - just to make sure they are there)
 sudo $PACKAGE_MANAGER -Syu --needed --noconfirm firefox vim kitty zsh rofi $($PACKAGE_MANAGER -Ssq texlive-*) signal-desktop telegram-desktop base-devel nemo rsync docker
-yay -Syu --noconfirm polybar arandr pulseaudio pavucontrol spotify nextcloud-client go-task pre-commit
+yay -Syu --noconfirm polybar arandr pulseaudio pavucontrol spotify nextcloud-client go-task pre-commit sioyek
 
 # docker
 sudo systemctl enable docker.socket
