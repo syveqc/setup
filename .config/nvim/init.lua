68976e18e20df4e0ed0fc2f49411cff93d9b87c7
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-22 08:24:55 +0200

    feat: set default nvim shell to zsh

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- set default shell to be zsh
+vim.o.shell = '/usr/bin/zsh'
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
