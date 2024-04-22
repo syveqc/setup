a7dfa902b9016574aa7fef30da52e6cb2220fd11
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-22 08:25:59 +0200

    feat: add toggleterm plugin to nvim

---
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,17 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  keys = {
+    { '<leader>td', '<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+  },
+  config = function()
+    require('toggleterm').setup()
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+  end,
+}
