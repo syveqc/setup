6d222d4f82499b793a8658be8cd2c6058d499548
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-22 13:57:45 +0200

    feat: add langmap for easier bracket access (working questionably)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -169,6 +169,9 @@ vim.keymap.set('n', '<leader>q', function()
   vim.cmd 'bd'
 end, { desc = '[Q]uit current buffer' })
 
+-- set langmap for easier normal mode experience
+vim.cmd 'set langmap=öäÖÄ;{}[]'
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
