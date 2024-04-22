ed1c26e7ffceb57780a7470787f87bd4e8251727
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-22 13:58:31 +0200

    change: terminal parameters

--- .config/nvim/lua/custom/plugins/toggleterm.lua
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   keys = {
-    { '<leader>td', '<cmd>ToggleTerm size=40 dir=~/Desktop direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>td', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
   },
   config = function()
     require('toggleterm').setup()
