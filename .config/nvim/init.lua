e5b38f4fdf5a3cb87945d581ad1e4b8d5aa32bdc
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-23 17:14:15 +0200

    feat: activate relative line numbers

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -106,7 +106,7 @@ vim.opt.autochdir = true
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
