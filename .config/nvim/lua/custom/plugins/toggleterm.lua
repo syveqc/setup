d2170f327efa3d4f3145b1f1d91ba2f014df37de
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-23 13:16:38 +0200

    feat: tabbed terminal

--- .config/nvim/lua/custom/plugins/toggleterm.lua
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -3,6 +3,7 @@ return {
   version = '*',
   keys = {
     { '<leader>td', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', desc = 'Open a horizontal terminal at the Desktop directory' },
+    { '<leader>tt', '<cmd>ToggleTerm size=20 direction=tab<cr>', desc = 'Open a tabbed terminal at the Desktop directory' },
   },
   config = function()
     require('toggleterm').setup()
