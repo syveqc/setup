f144249f0d4395a5504d246051b1e53b90d1b747
Tobias Kietreiber <tobias.kietreiber@fhstp.ac.at>
2024-04-24 09:28:02 +0200

    feat: move terminal keymappings from toggleterm to init.lua to have it available in normal terminals as well

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,6 +200,15 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- useful terminal navigation commands
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- .config/nvim/lua/custom/plugins/toggleterm.lua
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -7,12 +7,5 @@ return {
   },
   config = function()
     require('toggleterm').setup()
-    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
-    vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
-    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]])
-    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]])
-    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
-    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
-    vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
   end,
 }
