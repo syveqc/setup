bc056c7c97931e5c28ec2e02294bb806407b92f6
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-04-26 13:40:15 +0200

    feat: add toggleterm keymaps to terminal mode

--- .config/nvim/lua/custom/plugins/toggleterm.lua
+++ .config/nvim/lua/custom/plugins/toggleterm.lua
@@ -8,5 +8,8 @@ return {
   },
   config = function()
     require('toggleterm').setup()
+    vim.keymap.set('t', '<leader>td', '<cmd>ToggleTerm size=150 direction=vertical<cr>', { desc = 'Open a vertical terminal at the current directory' })
+    vim.keymap.set('t', '<leader>th', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', { desc = 'Open a horizontal terminal at the current directory' })
+    vim.keymap.set('t', '<leader>tt', '<cmd>ToggleTerm direction=tab<cr>', { desc = 'Open a tabbed terminal at the current directory' })
   end,
 }
