39bf2930e64cfa97ecb356d96e2e1ebf7281ed29
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2024-05-27 00:07:31 +0200

    feat: add some styling to treesitter-context

--- .config/nvim/lua/custom/plugins/treesitter-context.lua
+++ .config/nvim/lua/custom/plugins/treesitter-context.lua
@@ -1,6 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter-context',
   config = function()
+    require('treesitter-context').setup {
+      multiline_threshold = 3, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+    }
     vim.keymap.set('n', '[c', function()
       require('treesitter-context').go_to_context(vim.v.count1)
     end, { silent = true })
