00ef9354c1b0a123da132529ac2a845e6c3661f1
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2025-03-28 09:57:50 +0100

    feat: add nvim-notify

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -7,6 +7,7 @@ return {
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
+    'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup {
