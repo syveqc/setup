79045b4945a0b996627a2fa2f501b8fce4957fb6
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2026-05-19 21:14:01 +0200

    feat: add jk to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -210,6 +210,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('i', 'jk', '<Esc>')
+
 -- useful terminal navigation commands
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
