9c81d8ce1b41a35a385175584235339f0eff029e
Tobias Kietreiber <tobias.kietreiber@hotmail.com>
2026-05-19 20:45:11 +0200

    feat: change wezterm config to enable wayland

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.enable_wayland = true
+
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 12.0
+config.font_size = 10.0
 config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 
-config.color_scheme = "Rosé Pine Moon"
+config.color_scheme = "rose-pine-moon"
 
 config.enable_tab_bar = false
 config.window_padding = { left = 4, right = 4, top = 4, bottom = 4 }
